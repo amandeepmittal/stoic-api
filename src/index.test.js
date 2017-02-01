53491c3daf11b39ff2e98f14ad264f5204df5991
amandeepmittal <amandeepmittal@live.com>
2017-02-01 17:13:59 +0530

    update the api: major release

---
+++ src/index.test.js
@@ -0,0 +1,23 @@
+var expect = require('chai').expect;
+var stoicApi = require('./index');
+
+describe('stoicApi', function () {
+    describe('all', function () {
+        it('should be an array of strings', function () {
+            expect(stoicApi.all).to.satisfy(isArrayOfStrings);
+            
+            function isArrayOfStrings(array) {
+                return array.every(function (item) {
+                    return typeof item === 'string';
+                });
+            }
+        });
+    });
+
+    describe('random', function () {
+        it('should return a random item from stoicApi.all', function () {
+            var randomItem = stoicApi.random();
+            expect(stoicApi.all).to.include(randomItem);
+        });
+    });
+});
